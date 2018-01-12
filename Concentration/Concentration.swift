5cbfc1b8f6e3762bb670eeef631eb688389f705f
Ivan Tchernev <ivan.useful@gmail.com>
2018-01-12 13:48:34 +0000

    Introduced computed properties

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,9 +12,44 @@ class Concentration
 {
     var cards = [Card]()
     var score = 0
-    var isGameComplete = false
+    var isGameComplete: Bool {
+        get {
+            for card in cards {
+                if !card.isMatched {
+                    return false
+                }
+            }
+            return true
+        }
+    }
 
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                if(cards[index].isFaceUp) {
+                    //If we've had a repeated mismatch, decrease the score
+                    if !cards[index].isMatched && cards[index].hasBeenFlippedAtLeastOnce {
+                        score -= 1
+                    }
+                    cards[index].hasBeenFlippedAtLeastOnce = true
+                }
+                cards[index].isFaceUp = (index == newValue);
+            }
+        }
+    }
     
     init(numberOfCards: Int){
         let numberOfCardPairs = numberOfCards % 2 == 0 ? numberOfCards / 2 : (numberOfCards + 1) / 2
@@ -31,39 +66,17 @@ class Concentration
     
     func chooseCard(at index: Int){
         if !cards[index].isMatched {
-            //If indexOfOneAndOnlyFaceUpCard is set, then this is the 2nd card being turned over
-            //so we need to check for a match. Otherwise turn all cards face down
+            //If we have a faceup card check for a match. Otherwise, update the index.
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
                 if cards[matchIndex].identifier == cards[index].identifier {
                     cards[index].isMatched = true
                     cards[matchIndex].isMatched = true
                     score += 2
-                    checkIfGameComplete()
                 }
-                indexOfOneAndOnlyFaceUpCard = nil
+                cards[index].isFaceUp = true
             } else {
-                for flipDownIndex in cards.indices {
-                    if cards[flipDownIndex].isFaceUp {
-                        if !cards[flipDownIndex].isMatched && cards[flipDownIndex].hasBeenFlippedAtLeastOnce {
-                            score -= 1
-                        }
-                        cards[flipDownIndex].hasBeenFlippedAtLeastOnce = true
-                        cards[flipDownIndex].isFaceUp = false;
-                    }
-                }
                 indexOfOneAndOnlyFaceUpCard = index
             }
-            cards[index].isFaceUp = true
         }
-    }
-    
-    func checkIfGameComplete() {
-        for card in cards {
-            if !card.isMatched {
-                isGameComplete = false
-                return
-            }
-        }
-        isGameComplete = true
     }
 }
